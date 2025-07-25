623ca6dff6bffa9a1ee2f2a54824209549fc5830
Rohit0445 <meenarohit458@gmail.com>
2025-07-25 12:47:51 +0530

    commit

--- formhandling/src/components/Showdata.jsx
+++ formhandling/src/components/Showdata.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
 const Showdata = () => {
+
+    let show = JSON.parse(localStorage.getItem('userdata'))
+
+    function Delete(){
+        localStorage.clear()
+    }
   return (
    <>
 
    <h1>Hello......</h1>
+
+   <h1>username - {show.username}</h1>
+   <h1>Email - {show.email}</h1>
+   <h1>Password - {show.password}</h1>
+
+    <button onClick={Delete()} >Delete</button>
    
    
    </>
